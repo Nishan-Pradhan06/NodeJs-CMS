1bad38128e5a9c18170ebc8b6d5628d288074a12
nishan <nishanpradhan500@gmail.com>
2023-09-15 22:26:51 +0545

    connect to database

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,16 +1,16 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "blogs",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-};
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "blogs",
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000,
+//     },
+// };
 // module.exports = {
 //     HOST: "localhost",
 //     USER: "root",
@@ -24,3 +24,17 @@ module.exports = {
 //         idle: 10000,
 //     },
 // };
+
+module.exports = {
+    HOST: "containers-us-west-59.railway.app",
+    USER: "railway",
+    PASSWORD: "culXRaHy4OtFCYpZu0Ur",
+    DB: "blogs",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+};
